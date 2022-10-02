2a2f58c1f81df1f0092fff63209176dbcba1ba37
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-02 10:37:54 -0700

    made prettier

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
 // Dependencies
-let express = require('express')
-const methodOverride = require('method-override')
-const mongoose = require('mongoose')
-  
-
+let express = require("express");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 
 //Configuration
-require('dotenv').config()
-let PORT = process.env.PORT
-let app = express()
+require("dotenv").config();
+let PORT = process.env.PORT;
+let app = express();
 
-// Mongoose 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
+// Mongoose
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
 
 // Middleware
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
-
-
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 //Routes
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to an Awesome App about Breads!");
+});
 
 // Breads
-let breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+let breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
 
 // 404 Page
-app.get('*', (req, res) => {
-    res.send('404')
-  })
-  
+app.get("*", (req, res) => {
+  res.send("404");
+});
 
 //Listen
 app.listen(PORT, () => {
-    console.log('listening on port', PORT)
-})
+  console.log("listening on port", PORT);
+});
